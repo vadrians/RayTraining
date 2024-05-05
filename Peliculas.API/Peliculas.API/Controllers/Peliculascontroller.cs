b97f2cbd58864f6678027ca8771eb4c6c2a19f84
Ray <rayazcam@gmail.com>
2024-05-05 12:28:39 -0500

    Agregar clase

---
+++ Peliculas.API/Peliculas.API/Controllers/Peliculascontroller.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Peliculas.API.Dto;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Peliculas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        // GET: api/<Peliculascontroller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<Peliculascontroller>/5
+        [HttpGet("{id}")]
+        public  IActionResult  Get(int id)
+        {
+            return Ok(new Pelicula() {
+            
+            Nombre="Gladiador",Fecha=new DateTime(2004,4,22)});
+            
+        }
+
+        // POST api/<Peliculascontroller>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Peliculascontroller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Peliculascontroller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Peliculas.API/Peliculas.API/Dto/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peliculas.API.Dto
+
+{
+    public class Pelicula
+    {
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
